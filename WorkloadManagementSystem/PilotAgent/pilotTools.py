5ba7df1a23d32fd79c1168bbe26768438c5cc937
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-11-07 10:25:21 +0100

    Add gridVersion for pilots

--- WorkloadManagementSystem/PilotAgent/pilotTools.py
+++ WorkloadManagementSystem/PilotAgent/pilotTools.py
@@ -376,7 +376,7 @@ class PilotParams(object):
     self.userDN = ""
     self.maxCycles = self.MAX_CYCLES
     self.flavour = 'DIRAC'
-    self.gridVersion = ''
+    self.gridVersion = 'v14r3'
     self.pilotReference = ''
     self.releaseVersion = ''
     self.releaseProject = ''
