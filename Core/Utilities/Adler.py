646827ac3c2f646b6dfa6b7c7f7558d46885c934
ILCDIRAC <ilcdirac-admin@cern.ch>
2018-06-11 15:59:14 +0200

    Adler: silence warnings about checksums

--- Core/Utilities/Adler.py
+++ Core/Utilities/Adler.py
@@ -24,7 +24,7 @@ def intAdlerToHex( intAdler ):
     # Will always be 8 hex digits made from a positive integer
     return hex( intAdler & 0xffffffff ).lower().replace( 'l', '' ).replace( 'x', '0000' )[-8:]
   except Exception as error:
-    print repr( error ).replace( ',)', ')' )
+    #print repr( error ).replace( ',)', ')' )
     return False
 
 def hexAdlerToInt( hexAdler, pos = True ):
@@ -47,7 +47,7 @@ def hexAdlerToInt( hexAdler, pos = True ):
     # Will always try to return the positive integer value of the provided hex
     return int( hexAdler, 16 ) & 0xffffffff
   except Exception as error:
-    print repr( error ).replace( ',)', ')' )
+    #print repr( error ).replace( ',)', ')' )
     return False
 
 def compareAdler( adler1, adler2 ):
